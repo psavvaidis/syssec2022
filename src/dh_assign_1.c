c8945bbf12ec95a007df766fe03d75822246e9c0
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-10-31 14:25:31 +0200

    Finish implementing the algorithm and some basic checks

--- src/dh_assign_1.c
+++ src/dh_assign_1.c
@@ -6,12 +6,19 @@
 void help();
 
 int main(int argc, char *argv[]){
-    mpz_t p, g, a, b;
+    mpz_t p, g, a, b, secret_a, secret_b, a_sends, b_sends;
+    char* outputFile = NULL;
+
+    // initialize integers
     mpz_init(p);
     mpz_init(a);
     mpz_init(b);
     mpz_init(g);
-    char* outputFile = NULL;
+    mpz_init(secret_a);
+    mpz_init(secret_b);
+    mpz_init(a_sends);
+    mpz_init(b_sends);
+
 
     for (int i = 1; i < argc; i++){
         // print help
@@ -36,9 +43,30 @@ int main(int argc, char *argv[]){
             mpz_set_str(b, argv[++i],0);
         }
     }
+    if(mpz_cmp(a, p)>0){
+        printf("a must be smaller than p. try again.\n");
+        return -1;
+    }
+    if(mpz_cmp(b, p)>0){
+        printf("b must be smaller than p. try again.\n");
+        return -1;
+    }
 
-    
-    gmp_printf("%Zd", g);
+
+    // computing nums to be sent
+    mpz_powm(a_sends, g, a, p);
+    mpz_powm(b_sends, g, b, p);
+
+    // computing secret
+    mpz_powm(secret_a, b_sends, a, p);
+    mpz_powm(secret_b, a_sends, b, p);
+
+    if(mpz_cmp(secret_a, secret_b)==0){
+        printf("Secret was successfully shared\n");
+        gmp_printf("%Zd, %Zd\n", secret_a, secret_b);
+    }else{
+        printf("Error sharing secret\n");
+    }
 
     return 0;
 }
@@ -52,3 +80,4 @@ void help(){
     -b\tnumber\tPrivate key B\n\
     -h\t\tThis help message\n");
 }
+
