e341c59590b1202f1926bf8457fe526831d62970
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-10-31 20:07:06 +0200

    complete args handling and declare helperfunctions

--- src/rsa_assign_1.c
+++ src/rsa_assign_1.c
@@ -12,9 +12,49 @@ void help(){
     -h\t\tThis help message\n");
 }
 
-int main(int argc, char *argv[]){
+void encrypt();
+void decrypt();
+void generate();
 
-    
+int main(int argc, char *argv[]){
+    FILE *input_f = NULL, *output_f = NULL, *key_f = NULL;
+    int toEncrypt = 0;
+    int toDecrypt = 0;
+
+    for (int i = 1; i < argc; i++){
+        // print help
+        if(strcmp(argv[i],"-h") == 0){
+            help();
+            return 0;
+        }// get the output file name from arguments
+        else if(strcmp(argv[i],"-i")==0){
+            input_f = (char*)malloc(strlen(argv[++i]));
+            strcpy(input_f, argv[i]);
+        }// get the output file name from arguments
+        else if(strcmp(argv[i],"-o")==0){
+            output_f = (char*)malloc(strlen(argv[++i]));
+            strcpy(output_f, argv[i]);
+        }// get the output file name from arguments
+        else if(strcmp(argv[i],"-k")==0){
+            key_f = (char*)malloc(strlen(argv[++i]));
+            strcpy(key_f, argv[i]);
+        }else if(strcmp(argv[i],"-g")==0){
+            generate();
+            return 0;
+        }
+        else if(strcmp(argv[i],"-d")==0 && !toEncrypt){
+            toDecrypt = 1;
+        }
+        else if(strcmp(argv[i],"-e")==0 && !toDecrypt){
+            toEncrypt = 1;
+        }
+    }
+
+    if(toEncrypt){
+        encrypt();
+    }else if (toDecrypt){
+        decrypt();
+    }
 
     return 0;
 }
