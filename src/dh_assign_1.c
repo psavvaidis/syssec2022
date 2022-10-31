c9b8cf3bc480b4122bcbf54f7fe05d7900bb9b48
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-10-31 13:51:32 +0200

    finish arguments handling

--- src/dh_assign_1.c
+++ src/dh_assign_1.c
@@ -7,20 +7,38 @@ void help();
 
 int main(int argc, char *argv[]){
     mpz_t p, g, a, b;
+    mpz_init(p);
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(g);
     char* outputFile = NULL;
 
     for (int i = 1; i < argc; i++){
+        // print help
         if(strcmp(argv[i],"-h") == 0){
             help();
             return 0;
-        }else if(strcmp(argv[i],"-o")==0){
+        }// get the output file name from arguments
+        else if(strcmp(argv[i],"-o")==0){
             outputFile = (char*)malloc(strlen(argv[++i]));
             strcpy(outputFile, argv[i]);
+        }// get p value from arguments
+        else if(strcmp(argv[i],"-p")==0){
+            mpz_set_str(p, argv[++i],0);
+        }// get g value from arguments
+        else if(strcmp(argv[i],"-g")==0){
+            mpz_set_str(g, argv[++i],0);
+        }// get a value from arguments
+        else if(strcmp(argv[i],"-a")==0){
+            mpz_set_str(a, argv[++i],0);
+        }// get b value from arguments
+        else if(strcmp(argv[i],"-b")==0){
+            mpz_set_str(b, argv[++i],0);
         }
     }
 
     
-    printf("%s",outputFile);
+    gmp_printf("%Zd", g);
 
     return 0;
 }
