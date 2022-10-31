09f1cbf1d224d9b23de2f7a7146a9eb2505eb695
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-10-31 19:15:24 +0200

    Add Help Menu to RSA

---
+++ src/rsa_assign_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <gmp.h>
+
+void help(){
+    printf("Options:\n\
+    -i\tpath\tPath to input file\n\
+    -o\tpath\tPath to output file\n\
+    -k\tpath\tPath to key file\n\
+    -g\t\tPerform RSA key-pairgeneration\n\
+    -d\t\tDecrypt input and store results to output\n\
+    -e\t\tEncrypt input and store results to output\n\
+    -h\t\tThis help message\n");
+}
+
+int main(int argc, char *argv[]){
+
+    
+
+    return 0;
+}
+
